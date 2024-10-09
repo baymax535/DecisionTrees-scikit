ef42939396e34c61d5e18d8fbee8f393493561da
Labib Alam <labibalam1100@gmail.com>
2024-10-09 13:06:15 -0500

    Evaluation and classes through the tree

--- trainTree.py
+++ trainTree.py
@@ -1,6 +1,8 @@
 from sklearn.datasets import fetch_openml
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
+from sklearn import metrics
+
 
 #initialize
 
@@ -23,3 +25,20 @@ myTree.fit(house.data, house.target) #trained
 #prediction
 
 predictions = myTree.predict(house.data)
+
+print(type(predictions))
+print(predictions)
+
+#Evaluation
+
+g = metrics.accuracy_score(house.target, predictions)
+print(g) #1.0
+print(metrics.f1_score(house.target, predictions, pos_label="True")) #1.0
+print(metrics.precision_score(house.target, predictions, pos_label="False") #1.0
+)
+
+#Classess and probabilities
+print("CLASSES: \n")
+print(myTree.classes_)
+pp = myTree.predict_proba(house.data)
+print(pp)
